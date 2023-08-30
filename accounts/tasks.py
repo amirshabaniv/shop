17922bbef9e2afc219f0255512c5892b63cc4c0c
Amir Shabani <129328075+amirshabaniv@users.noreply.github.com>
2023-08-30 05:22:37 +0330

    Update tasks.py

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from accounts.models import OtpCode
 from pytz import timezone
 
+@shared_task
 def remove_expired_otp_codes():
     expired_time = datetime.now(tz=timezone('Asia/Tehran')) - timedelta(minutes=2)
     OtpCode.filter(created__lt=expired_time).delete()
