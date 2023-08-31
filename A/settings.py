4e51240017f88cdfb7c2a555fb1251463758977b
Amir Shabani <129328075+amirshabaniv@users.noreply.github.com>
2023-08-31 05:53:31 +0330

    Update settings.py

--- A/settings.py
+++ A/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jack',
         'USER': 'postgres',
-        'PASSWORD': 'amir4014',
+        'PASSWORD': 'your DB password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
